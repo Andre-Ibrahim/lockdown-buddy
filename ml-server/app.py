bb9903de016c4b186c96d7c726eaf32fbf256f7f
Jason Gerard <jasongerard321@gmail.com>
2022-01-22 13:44:34 -0500

    added basic prediction api

--- ml-server/app.py
+++ ml-server/app.py
@@ -1,5 +1,6 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import Flask, request
 
 chatbot = ChatBot('Ron Obvious')
 
@@ -9,7 +10,12 @@ trainer = ChatterBotCorpusTrainer(chatbot)
 # Train the chatbot based on the english corpus
 trainer.train("chatterbot.corpus.english")
 
-# Get a response to an input statement
-while True:
-    val = input("Enter your value: ")
-    print(chatbot.get_response(val))
+app = Flask("lockdown-buddy-predict")
+
+
+@app.route("/predict")
+def predict():
+    input = request.args.get('message')
+    prediction = chatbot.get_response("test")
+    print(input, prediction)
+    return str(prediction)
